ffa49ce30b723f68aadda6eb32a5eb5111862136
EC2 Default User <ec2-user@ip-172-31-17-21.ec2.internal>
2021-02-20 13:26:38 +0000

    이미지 다운로더 생성

--- AIServer.cpp
+++ AIServer.cpp
@@ -1,10 +1,25 @@
 
 #include <iostream>
-
+#include "ImageDownloader/ImageDownloader.h"
 using std::cout;
 using std::endl;
 
 int main()
 {
-    cout << "Print Test" << endl;
+	
+    ImageDownloader id;
+    id.Download("test.png", "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png");
+}
+
+
+inline void swapInt(int& a, int& b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+inline void swapBit(int& a, int& b)
+{
+	
 }
---
+++ ImageDownloader/ImageDownloader.h
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class ImageDownloader
+{
+public:
+    ImageDownloader();
+    void Download(const char* const savePath, const char* const downloadPath) const;
+};
+
+ImageDownloader::ImageDownloader()
+{
+    
+}
+
+void ImageDownloader::Download(const char* const savePath, const char* const downloadPath) const
+{
+    
+}
